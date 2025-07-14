0178dedd70db0ffcc0d5d977b7df6a615582e480
GIT-Saikat <saikatmondal2105@gmail.com>
2025-07-14 19:42:23 +0530

    Final change 9

---
+++ extra.ts
@@ -0,0 +1,5 @@
+
+    //   - run: |
+    //       echo "${{ secrets.SSH_PRIVATE_KEY }}" &> ~/ssh_key
+    //       chmod 700 /home/runner/ssh_key
+    //       ssh -o StrictHostKeyChecking=no -i ~/ssh_key root@64.227.147.124 -t "docker stop user_backend && docker run --name user_backend -d -p 8080:8080 saikat2105/user-app-backend:${{ github.sha }}"
